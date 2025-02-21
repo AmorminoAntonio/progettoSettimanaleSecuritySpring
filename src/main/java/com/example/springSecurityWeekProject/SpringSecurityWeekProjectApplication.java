80b3824a3c44cb1494a9b3898c4e131b3034f14b
Antonio Amormino <crisantoamo@gmail.com>
2025-02-21 14:42:12 +0100

    commit first structure

---
+++ src/main/java/com/example/springSecurityWeekProject/SpringSecurityWeekProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springSecurityWeekProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWeekProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWeekProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springSecurityWeekProject/SpringSecurityWeekProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springSecurityWeekProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWeekProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
